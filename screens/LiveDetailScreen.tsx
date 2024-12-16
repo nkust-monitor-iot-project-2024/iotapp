b98619e7cebe06af3bc98778dbb9379efa87f919
HoongYi1006 <howie088100@gmail.com>
2024-12-16 11:51:31 +0800

    Fix the live stream display

--- screens/LiveDetailScreen.tsx
+++ screens/LiveDetailScreen.tsx
@@ -1,62 +1,53 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
-import Video from 'react-native-video';
+import { useVideoPlayer, VideoView } from 'expo-video';
+import { useEvent } from 'expo';
+
+const videoSource = "https://hls-10319920-6785-4329-a29f-97593ad34500.zeabur.app/teststream/index.m3u8"
 
 export function LiveDetailScreen({ route }) {
   const { monitorName } = route.params;
   const [isRecording, setIsRecording] = useState(false);
 
-  const toggleRecording = () => {
-    setIsRecording((prev) => !prev);
-  };
+  const player = useVideoPlayer(videoSource, player => {
+    player.loop = true;
+    player.play();
+  });
+
+  const { isPlaying } = useEvent(player, 'playingChange', { isPlaying: player.playing });
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>{monitorName}</Text>
-      <View style={styles.liveView}>
-        {/* 替換模擬畫面為 HLS 播放器 */}
-        <Video
-          source={{ uri: 'https://hls-10319920-6785-4329-a29f-97593ad34500.zeabur.app/teststream/index.m3u8' }} // 替換成實際的 HLS URL
-          controls
-          style={styles.video}
-          resizeMode="contain"
-          onError={(error) => {
-            console.error('播放錯誤:', error);
+    <View style={styles.contentContainer}>
+      <VideoView style={styles.video} player={player} allowsFullscreen allowsPictureInPicture />
+      <View style={styles.controlsContainer}>
+        <Button
+          title={isPlaying ? 'Pause' : 'Play'}
+          onPress={() => {
+            if (isPlaying) {
+              player.pause();
+            } else {
+              player.play();
+            }
           }}
         />
       </View>
-      <Button
-        title={isRecording ? '停用智慧偵測' : '開始智慧偵測'}
-        onPress={toggleRecording}
-        color={isRecording ? 'red' : 'green'}
-      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  contentContainer: {
     flex: 1,
-    padding: 20,
-    backgroundColor: '#f0f0f0',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-    textAlign: 'center',
-  },
-  liveView: {
-    flex: 1,
-    marginBottom: 20,
-    backgroundColor: '#000',
-    borderRadius: 8,
-    justifyContent: 'center',
+    padding: 10,
     alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 50,
   },
   video: {
-    width: '100%',
-    height: '100%',
-    borderRadius: 8,
+    width: 350,
+    height: 275,
+  },
+  controlsContainer: {
+    padding: 10,
   },
 });
